86e85440c8a211e26298abb42c8e06a9536fd5b0
Anthony Giambrone <giambrone@accuratebiz.com>
2017-10-14 19:32:02 -0500

    new structure for album detail display. display image and additional album detail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const { title, artist, thumbnail_image } = album;
+
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+      <CardSection>
+        <View>
+          <Image
+            style={styles.thumbnail}
+            source={{ uri: thumbnail_image }}
+          />
+        </View>
+        <View style={styles.headerContent}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
   );
+};
+
+const styles = {
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  thumbnail: {
+    height: 50,
+    width: 50
+  }
 };
 
 export default AlbumDetail;
